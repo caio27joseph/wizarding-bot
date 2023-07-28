3489ae49eaf29eb0274410c0e1dba0fda1e9810e
Caio Joseph <caio27joseph@gmail.com>
2023-07-28 15:50:29 -0300

    update database settings

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,11 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'wizard.db',
-    entities: ['dist/**/*.entity.js'],
-    synchronize: true,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB_PATH || 'data/database.db',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+  ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
